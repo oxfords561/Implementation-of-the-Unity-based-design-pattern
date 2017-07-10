61912bec4fa6402e39e053819fe7bb781cc178f6
oxfords561 <18924192170@189.cn>
2017-07-10 12:05:05 +0800

    添加观察者模式的学习

---
+++ Assets/Scripts/观察者模式/PanelOne.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOne : MonoBehaviour {
+
+    private GameObject panelOne;
+    private GameObject panelTwo;
+
+    private void Start()
+    {
+        panelOne = transform.parent.GetChild(0).gameObject;
+        panelTwo = transform.parent.GetChild(1).gameObject;
+    }
+
+    public void BlueOnClick()
+    {
+        //分发事件,注意和接收者协议一致
+        NotificationCenter.Get().DispatchEvent("ChangeColor", Color.blue);
+    }
+
+    public void RedOnClick()
+    {
+        //分发事件,注意和接收者协议一致
+        NotificationCenter.Get().DispatchEvent("ChangeColor", Color.red);
+    }
+
+    public void GreenOnClick()
+    {
+        //分发事件,注意和接收者协议一致
+        NotificationCenter.Get().DispatchEvent("ChangeColor", Color.green);
+    }
+
+    public void NextOnClick()
+    {
+        if (panelOne != null && panelTwo != null)
+        {
+            panelOne.SetActive(false);
+            panelTwo.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/观察者模式/PanelTwo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelTwo : MonoBehaviour {
+
+    private GameObject panelOne;
+    private GameObject panelTwo;
+    private Text text;
+
+
+    private void Start()
+    {
+        panelOne = transform.parent.GetChild(0).gameObject;
+        panelTwo = transform.parent.GetChild(1).gameObject;
+        if(panelTwo != null)
+        {
+            text = panelTwo.transform.FindChild("Text").GetComponent<Text>();
+        }
+
+        //在接收者中注册事件及其回调方法
+        NotificationCenter.Get().AddEventListener("ChangeColor", ChangeColor);
+    }
+
+
+    public void LastOnClick()
+    {
+        if (panelOne != null && panelTwo != null)
+        {
+            panelOne.SetActive(true);
+            panelTwo.SetActive(false);
+        }
+    }
+
+    public void ChangeColor(Notification notific)
+    {
+        print("变换颜色");
+        text.color = (Color)notific.param;
+    }
+}
